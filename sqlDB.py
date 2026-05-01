ef9294f7b1534a99cfb87c0e8ccbeb0125030f48
jykcs95 <jykcs95@gmail.com>
2026-05-01 00:23:39 -0500

    Separates and store all the files in the directory Made it so it will change all the dta files inside of a directory to a corresponding cvs file It will make separate folders for different type of data stored in the dta file. Also, it will have a header on the top of each file depending on what type the data is.

--- sqlDB.py
+++ sqlDB.py
@@ -1,64 +1,102 @@
 import pandas as pd
-import sqlite3
 import gamry_parser as parser
 
+import sqlite3
+import glob
+from pathlib import Path
+
 #Converts dta files to csv files
-def dtaParser(s):
+def dtaParser(file):
+    #store the name of the file for later use
+    file_name = file.rsplit(".",1)[0]
+
+    #Call the gamry parser to read the dta files and get all the curve points
     gp = parser.GamryParser()
-    gp.load(s)
+    gp.load(file)
     curve_count = gp.get_curve_count()
+
+    #store all headers
     header = gp.get_header()
 
-    #create a csv document for the header
-    createHeader(gp.get_header())
+    #Get the correct query for each title where first index contains query and second index contains the type
+    title = header["TITLE"]
+    queryType = titleQuery(title)
     
-    #create multiple files depending on how many curves it currently has
+    #Send all the data in to reading functino to find the corresponding output using the query
     for i in range(curve_count):
-        curve_data = gp.get_curve_data(i)
-        curve_data.to_csv(f'data{i + 1}.csv', index=False)
-
-#Saving the Header info to the csv document
-def createHeader(s):
-    df=pd.DataFrame(s)
-    df.to_csv('header.csv', index=False)
+        curve_count = gp.get_curve_data(i)
+        path = reading(curve_count, queryType[0], file_name, queryType[1])
     
+    createHeader(path, header, file_name)
+
+#Finding the right query and return the tag as well  
+def titleQuery(title):
+    query = ""
+    match title.lower():
+        case "chronoamperometry scan":
+            query = "SELECT T, Vf, Im FROM 'my_table'"
+            tag = "CA"
+        case "cyclic voltammetry":
+            query = "SELECT VF, Im FROM 'my_table'"
+            tag = "CV"
+    return [query,tag]
+
+#Creating header using the "ignore the grid" way
+def createHeader(csvFile, header, fileName):
+    #open the csvfile
+    with open(csvFile,'r') as f:
+        existing_content= f.read()
+
+    #Get the right header for corresponding title
+    match header["TITLE"].lower():
+        case "chronoamperometry scan":
+            extra_data= ["File Name", fileName]
+        case "cyclic voltammetry":
+            extra_data= ["File Name", fileName,"SCAN RATE", header["SCANRATE"]]    
+    new_line = ",".join(map(str,extra_data)) + "\n"
+    
+    #rewrite the header with the existing content to the csv file
+    with open(csvFile,'w') as f:
+        f.write(new_line)
+        f.write(existing_content)
 
 #Using SQL queries to read and alter the csv file to create a new file that contains a table with all the requested information
-def reading(dataFile, sqlQuery):
-    df=pd.read_csv(dataFile)
+def reading(df, sqlQuery, outFile, outType):
+    #connect to sqlite3 in order to use sql query
     conn = sqlite3.connect(":memory:")
+    
     df.to_sql("my_table", conn, index=False)
 
     #SQL query to pull the necessary data from the csv  file
     query = sqlQuery
 
     result_df =  pd.read_sql_query(query, conn)
-    result_df.to_csv(f"output_{dataFile}", index = False)
+
+    #storing the output into a new folder in the directory
+    file_path = Path(f"results_{outType}/output_{outFile}.csv")
+    #create parent directories if they don't exist
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+
+    #add on to the file instead of rewriting it
+    result_df.to_csv(file_path, mode="a",index = False)
 
     conn.close()
 
-#It's going to go through the header file and search for its Tag and date
+    return file_path
+
+#It's going to go through the header file and search for its title and date
 def searchHeader(headerFile):
     df = pd.read_csv(headerFile)
-    tag = df.at[0,"TAG"]
+    title = df.at[0,"TITLE"]
     date = df.at[0,"DATE"]
-    return {"tag":tag, "date":date}
+    return {"title":title, "date":date}
+
+def getDTA():
+    return glob.glob("*.dta")
+
 
 if __name__ == "__main__":
 
-    #Name of the dta file
-    dtaFile = "data.dta"
-
-    query = ""
-    dtaParser(dtaFile)
-
-    #Header information - tag, date
-    headerInfo = searchHeader("header.csv")
-    match headerInfo['tag']:
-        case 'CA':
-            query = "SELECT T, Vf, Im FROM 'my_table'"
-        case 'CV':
-            query = "SELECT VF, Im FROM 'my_table'"
-    
-    reading("data1.csv",query)
-
+    dta_files = getDTA()
+    for file in dta_files:
+        dtaParser(file)
